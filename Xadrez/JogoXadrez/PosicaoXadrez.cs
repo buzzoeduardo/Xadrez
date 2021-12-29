92abf1cc56c41b84b100da5cd5be9bfd41613b27
Eduardo Buzzo <buzzorpg2201@gmail.com>
2021-12-29 20:12:50 -0300

    Melhorando a implementação do método imprimir tabuleiro

--- Xadrez/JogoXadrez/PosicaoXadrez.cs
+++ Xadrez/JogoXadrez/PosicaoXadrez.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tabuleiro;
 
-namespace Xadrez.JogoXadrez
+namespace JogoXadrez
 {
     class PosicaoXadrez
     {
+        public char Coluna { get; set; }
+        public char Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, char linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
     }
 }
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -11,18 +11,36 @@ namespace Xadrez
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int x = 0; x < tab.Colunas; x++)
-                {
+                {                    
                     if (tab.peca(i, x) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, x) + " ");
+                        ImprimirPeca(tab.peca(i, x));
+                        Console.Write(" ");
                     }                   
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor corSystem = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(peca);
+                Console.ForegroundColor = corSystem;
             }
         }
     }
